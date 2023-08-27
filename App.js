8b63e729e1435e1acf3af24dbad362c79c0379c6
Joonas Montonen <joonas.montonen@outlook.com>
2023-08-27 12:38:43 +0300

    eka palautus

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Button, Alert, TextInput, Image } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text } from "react-native";
 
 export default function App() {
+  const buttonPressed = () => {
+    Alert.alert("You typed:" + text);
+  };
+
+  const [text, setText] = useState("");
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+  });
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={{ width: 200, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => setText(text)}
+        value={text}
+      />
+
+      <Button onPress={buttonPressed} title="Press me" />
+      <Image
+        style={{ width: 250, height: 100 }}
+        source={require("assets\haaga_helia.jpg")} />
+      
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+
+
